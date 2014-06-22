59e32bba812cce6df2af4d87eeda01a708d9e411
yijiu19 <yijiu.zheng@gmail.com>
2014-06-22 14:50:40 -0700

    changed numeric() to matrix() on cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,7 @@
 
 ## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = numeric()) {
+makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
     set <- function(y) {
         x <<- y
